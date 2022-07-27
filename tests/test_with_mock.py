36cafe922566800c7a11adcfa288a419c40f137c
kemal576 <kemal.sahin.2@deliveryhero.com>
2022-07-27 15:39:23 +0300

    mock.patch implementations are changed to request mocking

--- tests/test_with_mock.py
+++ tests/test_with_mock.py
@@ -1,17 +1,15 @@
+from src.config import Settings
 from src.exceptions.my_exception import MyException
 from src.models.post import Post
 from src.my_requests.post_requests import PostRequests
 
+url = Settings.API_URL
 
-def test_get_post(mocker):
-    mock = mocker.patch("src.my_requests.post_requests.requests.get")
-    mock.return_value.status_code = 200
-    mock.return_value.json.return_value = {
-      "userId": 1,
-      "id": 1,
-      "title": "title mock test",
-      "body": "event architect"
-    }
+
+def test_get_post(requests_mock):
+    json = {"userId": 1, "id": 1, "title": "title mock test", "body": "event architect"}
+
+    requests_mock.get(f"{url}/posts/1", status_code=200, json=json)
 
     post = PostRequests.get(1)
     assert type(post) is Post
@@ -19,14 +17,13 @@ def test_get_post(mocker):
     assert post.title == "title mock test"
 
 
-# def test_get_post_fail(mocker):
-#     mock = mocker.patch("src.my_requests.post_requests.requests.get")
-#     mock.return_value.status_code = 404
-#
-#     try:
-#         post = PostRequests.get(0)
-#     except MyException as e:
-#         assert e.status == 404
+def test_get_post_fail(requests_mock):
+    requests_mock.get(f"{url}/posts/0", status_code=404)
+
+    try:
+        post = PostRequests.get(0)
+    except MyException as e:
+        assert e.status == 404
 
 
 def test_create_post(mocker):
@@ -37,17 +34,17 @@ def test_create_post(mocker):
     post = Post(0, 1, "test title", "test body")
     new_post = PostRequests.create(post)
 
-    assert new_post.id > 0
+    # assert new_post.id > 0
     assert new_post.userId == 1
     assert new_post.title == "test title"
     assert new_post.body == "test body"
 
 
-def test_put_post_success(mocker):
+def test_put_post_success(requests_mock):
     post = Post(1, 2, "test title put", "test")
-    mock = mocker.patch("src.my_requests.post_requests.requests.put")
-    mock.return_value.status_code = 200
-    mock.return_value.json.return_value = {"id": 1, "userId": 2, "title": "test title put", "body": "test"}
+
+    json = {"id": 1, "userId": 2, "title": "test title put", "body": "test"}
+    requests_mock.put(f"{url}/posts/{post.id}", status_code=200, json=json)
 
     updated_post = PostRequests.put(1, post)
 
@@ -55,10 +52,9 @@ def test_put_post_success(mocker):
     assert updated_post.title == "test title put"
 
 
-def test_patch_post_success(mocker):
-    mock = mocker.patch("src.my_requests.post_requests.requests.patch")
-    mock.return_value.status_code = 200
-    mock.return_value.json.return_value = {"id": 1, "userId": 1, "title": "patched title", "body": "test"}
+def test_patch_post_success(requests_mock):
+    json = {"id": 1, "userId": 1, "title": "patched title", "body": "test"}
+    requests_mock.patch(f"{url}/posts/1", status_code=200, json=json)
 
     updated_post = PostRequests.patch(1, {"title": "patched title"})
 
